f1ea187e73be2dd2730625aae6bbe8900371e3a4
Stuart P. Bentley <stuart@testtrack4.com>
2013-09-11 02:26:43 -0400

    Beginnings of a site framework

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+
+module.exports = function appctor(cfg) {
+  var app = express();
+
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+
+  app.use('/images',express.static(__dirname+'/images'));
+  app.use('/scripts',express.static(__dirname+'/scripts'));
+  app.use(express.static(__dirname+'/www'));
+
+  app.get('/', function(req,res) {
+    res.render('index.jade');
+  });
+};
